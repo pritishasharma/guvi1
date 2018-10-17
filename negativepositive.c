d29f0ba75400773ec3fba597a88363c3c11ec373
pritishasharma <44234219+pritishasharma@users.noreply.github.com>
2018-10-17 19:08:43 +0530

    Create negativepositive.c

---
+++ negativepositive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   printf("enter a no");
+   scanf("%d",&n);
+   if(n>0)
+   {
+       printf("positive");
+   }
+   else if(n==0)
+   {printf("zero");
+   }
+   else
+   {
+       printf("negative")
+       }
+    return 0;
+}
+
+
